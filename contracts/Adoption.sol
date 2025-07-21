6894e714ca720b564e164833c453268224f18d1d
3956ray <3956ray@gmail.com>
2025-07-21 16:55:46 +0800

    Adoption contract

---
+++ contracts/Adoption.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.5.16;
+
+contract Adoption {
+
+    address[16] public adopters;
+
+    // adding a pet
+    function adopt(uint petId) public returns (uint) {
+        require(petId >= 0 && petId <= 15);
+
+        adopters[petId] = msg.sender;
+
+        return petId;
+    }
+
+    // retrieving the adopters
+    function getAdopters() public view returns (address[16] memory) {
+        return adopters;
+    }
+
+
+}
